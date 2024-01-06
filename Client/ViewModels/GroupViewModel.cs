978fbdcec50ffa1414ddf3dccc99625ab98f9306
Lord Turmoil <tony-turmoil@outlook.com>
2024-01-06 16:09:36 +0800

    feat: view procedure

--- Client/ViewModels/GroupViewModel.cs
+++ Client/ViewModels/GroupViewModel.cs
@@ -4,6 +4,7 @@ using Client.Extensions.Popup;
 using Client.Services;
 using Prism.Events;
 using Prism.Regions;
+using Server.Modules.Core.Dtos;
 using Shared.Dtos;
 using Tonisoft.AspExtensions.Response;
 
@@ -12,8 +13,10 @@ namespace Client.ViewModels;
 class GroupViewModel : NavigationViewModel
 {
     private ObservableCollection<GroupDto> _allGroups = null!;
+    private ObservableCollection<ProcedureDto> _allProcedures = null!;
     private readonly IGroupService _groupService;
     private readonly IProcedureService _procedureService;
+    private GroupDto? _selectedGroup;
 
     public GroupViewModel(IEventAggregator eventAggregator, IGroupService groupService, IProcedureService procedureService)
         : base(eventAggregator)
@@ -29,13 +32,37 @@ class GroupViewModel : NavigationViewModel
         set => SetProperty(ref _allGroups, value);
     }
 
-    public GroupDto? SelectedPart { get; set; }
+    public GroupDto? SelectedGroup {
+        get => _selectedGroup;
+        set {
+            if (SetProperty(ref _selectedGroup, value))
+            {
+                OnSelectedGroupChanged(value);
+            }
+        }
+    }
 
+    public ObservableCollection<ProcedureDto> AllProcedures {
+        get => _allProcedures;
+        set => SetProperty(ref _allProcedures, value);
+    }
+
+    public ProcedureDto? SelectedProcedure { get; set; }
 
     public override void OnNavigatedTo(NavigationContext navigationContext)
     {
         base.OnNavigatedTo(navigationContext);
         FetchAllGroups();
+    }
+
+    private void OnSelectedGroupChanged(GroupDto? value)
+    {
+        if (value == null)
+        {
+            return;
+        }
+
+        AllProcedures = new ObservableCollection<ProcedureDto>(value.Procedures);
     }
 
     private async void FetchAllGroups()
